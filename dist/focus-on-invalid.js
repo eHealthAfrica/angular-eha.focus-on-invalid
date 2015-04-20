80f2f123817f9ffeef68e43624d37a6609c94151
danse <f.occhipinti@gmail.com>
2015-04-20 12:21:06 +0200

    Release v0.0.1

---
+++ dist/focus-on-invalid.js
@@ -0,0 +1,43 @@
+;(function() {
+  'use strict';
+  /**
+   * @ngdoc directive
+   * @name ehaFocusOnInvalid
+   * @module eha.focus-on-invalid
+   */
+  var ngModule = angular
+                  .module('eha.focus-on-invalid.directive', []);
+
+  ngModule.directive('ehaFocusOnInvalid', function() {
+    return {
+      restrict: 'A',
+      link: function postLink(scope, element) {
+        element.on('submit', function() {
+          var firstInvalid = element[0].querySelector('.ng-invalid');
+          if (firstInvalid) {
+            firstInvalid.focus();
+          }
+        });
+      }
+    };
+  });
+
+  // Check for and export to commonjs environment
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = ngModule;
+  }
+
+})();
+
+;(function() {
+
+  var ngModule = angular.module('eha.focus-on-invalid', [
+    'eha.focus-on-invalid.directive'
+  ]);
+
+  // Check for and export to commonjs environment
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = ngModule;
+  }
+
+})();
